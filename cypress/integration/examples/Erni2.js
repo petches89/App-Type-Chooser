1c70dbf48626d259c2588cd61249f5f0aeb373db
Adamcova Petronela <Petronela.Adamcova@swisscom.com>
2023-09-25 17:10:23 +0200

    fix unnecessary cypress import

--- cypress/integration/examples/Erni2.js
+++ cypress/integration/examples/Erni2.js
@@ -1,18 +1,15 @@
 /// <reference types="Cypress" />
 
-describe('App Chooser Test', () => 
-{
-
-    it('Test 1', () => 
-    {
+describe('App Chooser Test', () => {
+  it('Test 1', () => {
 
     cy.visit("https://betterask.erni/apptype-chooser/")
     cy.get(".btn").click()
     cy.get(':nth-child(1) > .form-check-label').click()
     cy.ClickNext()
-    
-    for(var i=0;i<11;i++){
-    cy.ClickNext()
+
+    for (var i = 0; i < 11; i++) {
+      cy.ClickNext()
     }
 
     // cy.get('#name').type('Petronela')
@@ -21,17 +18,9 @@ describe('App Chooser Test', () =>
     // cy.get('#phone').type('+41765356089')
     // cy.ClickNext()
 
-    cy.on('window:alert', (str)=>
-    {
+    cy.on('window:alert', (str) => {
       expect(str).to.equal('Please answer at least 6 questions to get a meaningful result.')
     })
-
-    //cy.FillUp()
-
-  
-    
-    
-  
 
   })
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -13,27 +13,22 @@
 // Cypress.Commands.add('login', (email, password) => { ... })
 //
 
-const cypress = require("cypress")
-
 //we assigned a name of our command ('selectProduct') in which we wil give instructions for that command to handle the logic (passed the product name so it will click on the add to cart button)
 //productName - parameter that I passed to my product
 //this command accepts an argument called productName. The productName whatever you pass from your test will come into this command and you will replace it with your Blackberry
-Cypress.Commands.add('selectProduct', (productName) => { 
-    cy.get('h4.card-title').each(($el, index, $list) => {
-        if($el.text().includes(productName))
-        {
-          cy.get('button.btn.btn-info').eq(index).click()
-        }        
-    
-    
-      })
-})
+Cypress.Commands.add('selectProduct', (productName) => {
+  cy.get('h4.card-title').each(($el, index, $list) => {
+    if ($el.text().includes(productName)) {
+      cy.get('button.btn.btn-info').eq(index).click()
+    }
 
+
+  })
+})
 
 Cypress.Commands.add('ClickNext', () => {
-cy.get('.text-right > .btn').click()
+  cy.get('.text-right > .btn').click()
 })
-
 
 Cypress.Commands.add('SelectFirst', () => {
   cy.get('.form-check-label').eq(0).click()
@@ -43,24 +38,11 @@ Cypress.Commands.add('NthHeader', (index) => {
   return cy.get(':nth-child(' + index + ') > .card > .card-header > .row > .col-11 > h3');
 })
 
-
 Cypress.Commands.add('NthHeaderAssert', (index, text, percent) => {
   var header = cy.get(':nth-child(' + index + ') > .card > .card-header > .row > .col-11 > h3');
   header.should('include.text', text)
   header.children().eq(0).should('include.text', percent + '%')
 })
-
-
-
-// cypress.Commands.add('FillUp', ()=>
-// {
-//     cy.get('#name').type('Petronela')
-//     cy.get('#mailAddress').type('test@gmail.com')
-//     cy.get('#company').type('TestingGmbH')
-//     cy.get('#phone').type('+41765356089')
-// })
-
-
 
 
 //
